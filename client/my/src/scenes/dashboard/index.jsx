d314d8f714691752986686a5550e8676b1c1607c
Dipak99990 <113429795+Dipak99990@users.noreply.github.com>
2023-03-24 23:33:19 +0545

    second commit

---
+++ client/my/src/scenes/dashboard/index.jsx
@@ -0,0 +1,29 @@
+
+import React from "react";
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+
+
+
+const Dashboard = () => {
+  return (
+   <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+  )
+}
+
+export default Dashboard
